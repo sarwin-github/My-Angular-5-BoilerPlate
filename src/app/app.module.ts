f6b1bcd57624647364838edb7e114d5d7719b633
Sherwin Dapito <dapito.sherwin@yahoo.com>
2018-05-01 11:36:22 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { HomeRoutingModule } from './home/home.routing.module';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
